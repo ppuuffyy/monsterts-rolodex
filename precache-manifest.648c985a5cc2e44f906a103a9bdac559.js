6fb85c5fb8ee28e8f037736e5327d0ef0051b2c2
ppuuffyy <ppuuffyy@gmail.com>
2020-04-05 17:47:03 +0300

    Updates

---
+++ precache-manifest.648c985a5cc2e44f906a103a9bdac559.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f888c33890bba9614a2896927309364",
+    "url": "/monsterts-rolodex/index.html"
+  },
+  {
+    "revision": "ecac9619ca0df7524870",
+    "url": "/monsterts-rolodex/static/css/main.3399b8ea.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsterts-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsterts-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ecac9619ca0df7524870",
+    "url": "/monsterts-rolodex/static/js/main.e3f214e8.chunk.js"
+  },
+  {
+    "revision": "941f549d785632a7ccad",
+    "url": "/monsterts-rolodex/static/js/runtime-main.cd5356a5.js"
+  }
+]);
